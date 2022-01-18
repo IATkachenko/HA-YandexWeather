79a91e688278b30a32f87b255e8c60af2be322f6
Igor Tkachenko <IATkachenko@gmail.com>
2022-01-18 14:41:00 +0300

    test: migrate to pytest

--- tests/functional/test_translations.py
+++ tests/functional/test_translations.py
@@ -1,39 +1,60 @@
 from __future__ import annotations
-import unittest
+import pytest
 import sys
 import os
 import json
+from functools import cache, cached_property
 
 sys.path.append(os.path.join(sys.path[0], "../../custom_components"))
 
 
-class TranslationsTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
+class Translations:
+    def __init__(self):
+        self.config: list[str] = []
+        self.weather: list[str] = []
+        self.sensor: list[str] = []
+
+        for i in os.listdir(self.location):
+            if i.startswith('sensor'):
+                self.sensor.append(i)
+            elif i.startswith('weather'):
+                self.weather.append(i)
+            else:
+                self.config.append(i)
+
+    @cached_property
+    def location(self) -> str:
         p = str(sys.path[0].replace("\\", "/"))
 
         if p.split("/")[-1] == "functional":
             p = os.path.join(sys.path[0], '../..')
 
-        cls._translations_location = os.path.normpath(os.path.join(
+        result = os.path.normpath(os.path.join(
             p,
             "custom_components/yandex_weather/translations"
         ))
-        cls._config = []
-        cls._weather = []
-        cls._sensor = []
 
-        for i in os.listdir(cls._translations_location):
-            if i.startswith('sensor'):
-                cls._sensor.append(i)
-            elif i.startswith('weather'):
-                cls._weather.append(i)
-            else:
-                cls._config.append(i)
+        return result
 
+    @cached_property
+    def en(self) -> str:
+        return f"{self.location}/en.json"
+
+
+@cache
+def get_translations() -> Translations:
+    return Translations()
+
+
+@pytest.fixture()
+def translations():
+    return Translations()
+
+
+class TestTranslations:
     def _have_en(self, where: []):
         """Make sure that we have English language file"""
-        self.assertIn('en.json', where)
+        assert 'en.json' in where
 
     def _compare_keys(self, d1: {}, d2: {}) -> list[str] | None:
         """
@@ -50,47 +71,40 @@ class TranslationsTest(unittest.TestCase):
                     return f"{k}/{s}"
         return None
 
-    def test_config_all_defined(self):
+    @pytest.mark.parametrize("translation", get_translations().config)
+    def test_config_all_defined(self, translations, translation):
         """Make sure thar translation have all fields like English version"""
+        if translation == 'en.json':
+            pytest.skip(f"Skipping {translation} because we should compare it with itself")
 
-        en_name = 'en.json'
-        with open(f"{self._translations_location}/{en_name}") as en:
+        with open(translations.en) as en:
             en_json = json.load(en)
-            for t in self._config:
-                if t == en_name:  # skip English itself
-                    continue
-                with self.subTest(translation=t.removesuffix('.json')):
-                    with open(f"{self._translations_location}/{t}") as tr:
-                        tr_json = json.load(tr)
+            with open(f"{translations.location}/{translation}") as tr:
+                tr_json = json.load(tr)
+                if (d := self._compare_keys(en_json, tr_json)) is not None:
+                    pytest.fail(f"{d} is missed for {t}")
 
-                        if (d := self._compare_keys(en_json, tr_json)) is not None:
-                            self.fail(f"{d} is missed for {t}")
-
-    def test_config_same_values(self):
+    @pytest.mark.parametrize("translation", get_translations().config)
+    def test_config_same_values(self, translations, translation):
         """Make sure that we have same description for config and options dialogs"""
-        for i in self._config:
-            with open(f"{self._translations_location}/{i}") as f:
-                content = json.load(f)
-                c_keys = content['config']['step']['user']['data'].keys()
-                o_keys = content['options']['step']['init']['data'].keys()
-                for k in set(c_keys).intersection(o_keys):
-                    with self.subTest(translation=i.removesuffix('.json'), key=k):
-                        config_value = content['config']['step']['user']['data'].get(k)
-                        option_value = content['options']['step']['init']['data'].get(k)
-                        self.assertEqual(config_value, option_value)
+        with open(f"{translations.location}/{translation}") as f:
+            content = json.load(f)
+            c_keys = content['config']['step']['user']['data'].keys()
+            o_keys = content['options']['step']['init']['data'].keys()
+            for k in set(c_keys).intersection(o_keys):
+                config_value = content['config']['step']['user']['data'].get(k)
+                option_value = content['options']['step']['init']['data'].get(k)
+                assert config_value == option_value
 
-    def test_config_have_en(self):
-        self._have_en(self._config)
+    def test_config_have_en(self, translations):
+        self._have_en(translations.config)
 
-    def test_weather_have_en(self):
-        if len(self._weather) == 0:
-            self.skipTest("Have no weather translations at all")
-        self._have_en(self._weather)
+    def test_weather_have_en(self, translations):
+        if len(translations.weather) == 0:
+            pytest.skip("Have no weather translations at all")
+        self._have_en(translations.weather)
 
-    def test_sensor_have_en(self):
-        if len(self._sensor) == 0:
-            self.skipTest("Have no sensor translations at all")
-        self._have_en(self._sensor)
-
-
-
+    def test_sensor_have_en(self, translations):
+        if len(translations.sensor) == 0:
+            pytest.skip("Have no sensor translations at all")
+        self._have_en(translations.sensor)
