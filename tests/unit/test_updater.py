3258a1917671970e19c88d83d5a6c861ca0f1f2f
IATkachenko <IATkachenko@gmail.com>
2022-01-24 21:24:19 +0300

    test(updater): add device_id

--- tests/unit/test_updater.py
+++ tests/unit/test_updater.py
@@ -5,6 +5,6 @@ from custom_components.yandex_weather.updater import WeatherUpdater
 
 async def test_update(hass, _bypass_get_data):
     """Test update action."""
-    w = WeatherUpdater(0, 0, "", hass)
+    w = WeatherUpdater(0, 0, "", hass, "test_device")
     await w.async_request_refresh()
     print(w)
