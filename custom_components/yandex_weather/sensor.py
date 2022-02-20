c8e3d5c1b4994b5f0902342262dfb34788fbc66f
IATkachenko <IATkachenko@gmail.com>
2022-02-20 14:26:06 +0300

    fix(sensor): added icon for wind speed

--- custom_components/yandex_weather/sensor.py
+++ custom_components/yandex_weather/sensor.py
@@ -65,6 +65,7 @@ WEATHER_SENSORS: tuple[SensorEntityDescription, ...] = (
         native_unit_of_measurement=SPEED_METERS_PER_SECOND,
         state_class=SensorStateClass.MEASUREMENT,
         entity_registry_enabled_default=False,
+        icon="mdi:weather-windy",
     ),
     SensorEntityDescription(
         key=ATTR_API_WIND_BEARING,
