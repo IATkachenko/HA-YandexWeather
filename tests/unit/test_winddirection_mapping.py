578c866d3628b37426ecef5e7ed969c310d40890
IATkachenko <IATkachenko@gmail.com>
2022-05-10 16:38:32 +0300

    test(WIND_DIRECTION_MAPPING): "c" wind direction is mapped to 0

--- tests/unit/test_winddirection_mapping.py
+++ tests/unit/test_winddirection_mapping.py
@@ -7,4 +7,4 @@ from custom_components.yandex_weather.updater import WIND_DIRECTION_MAPPING
 async def test_mapping():
     """Test mapping action."""
     assert 270 == map_state(src="w", mapping=WIND_DIRECTION_MAPPING)
-    assert map_state(src="c", mapping=WIND_DIRECTION_MAPPING) is None
+    assert 0 == map_state(src="c", mapping=WIND_DIRECTION_MAPPING)
