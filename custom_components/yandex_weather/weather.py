f77a94406000690472d0a8425ac7a7f8ce385419
IATkachenko <IATkachenko@gmail.com>
2022-07-21 01:36:19 +0300

    fix(weather): Yandex report pressure in HPA, not PA

--- custom_components/yandex_weather/weather.py
+++ custom_components/yandex_weather/weather.py
@@ -16,7 +16,7 @@ from homeassistant.components.weather import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     LENGTH_MILLIMETERS,
-    PRESSURE_PA,
+    PRESSURE_HPA,
     SPEED_METERS_PER_SECOND,
     TEMP_CELSIUS,
 )
@@ -65,7 +65,7 @@ class YandexWeather(WeatherEntity, CoordinatorEntity, RestoreEntity):
 
     _attr_attribution = ATTRIBUTION
     _attr_native_wind_speed_unit = SPEED_METERS_PER_SECOND
-    _attr_native_pressure_unit = PRESSURE_PA
+    _attr_native_pressure_unit = PRESSURE_HPA
     _attr_native_temperature_unit = TEMP_CELSIUS
     _attr_native_precipitation_unit = LENGTH_MILLIMETERS
     coordinator: WeatherUpdater
