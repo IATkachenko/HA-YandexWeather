4f287069c1de64967b9760fbdc5f2100a1e3993c
Igor Tkachenko <IATkachenko@gmail.com>
2023-03-28 17:37:37 +0300

    test: use `_bypass_get_data` automatically

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 from pytest_homeassistant_custom_component.common import load_fixture
 
 
-@pytest.fixture(name="_bypass_get_data")
+@pytest.fixture(name="_bypass_get_data", autouse=True)
 def bypass_get_data_fixture():
     """Skip calls to get data from API."""
     with patch(
