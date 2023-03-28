82d5ba3e570b3cac70f46b56f882c19a69abb8f9
Igor Tkachenko <IATkachenko@gmail.com>
2023-03-28 17:40:36 +0300

    test: add `@pytest.mark.asyncio` for async tests

--- tests/unit/test_updater.py
+++ tests/unit/test_updater.py
@@ -1,8 +1,10 @@
 """Tests for updater."""
+import pytest
 
 from custom_components.yandex_weather.updater import WeatherUpdater
 
 
+@pytest.mark.asyncio
 async def test_update(hass):
     """Test update action."""
     w = WeatherUpdater(0, 0, "", hass, "test_device")
--- tests/unit/test_winddirection_mapping.py
+++ tests/unit/test_winddirection_mapping.py
@@ -1,9 +1,11 @@
 """Tests for updater."""
+import pytest
 
 from custom_components.yandex_weather.const import map_state
 from custom_components.yandex_weather.updater import WIND_DIRECTION_MAPPING
 
 
+@pytest.mark.asyncio
 async def test_mapping():
     """Test mapping action."""
     assert 270 == map_state(src="w", mapping=WIND_DIRECTION_MAPPING)
