e3f3b4c25e4be01c9ecbcfda9a48724ea8ce4c3e
Igor Tkachenko <IATkachenko@gmail.com>
2023-03-28 18:15:35 +0300

    test(get_image): add test for Yandex as image source

--- tests/unit/test_get_image.py
+++ tests/unit/test_get_image.py
@@ -1,7 +1,12 @@
 """Tests for const::get_image."""
 import pytest
 
-from custom_components.yandex_weather.const import get_image
+from custom_components.yandex_weather.const import (
+    ATTR_API_IMAGE,
+    ATTR_API_ORIGINAL_CONDITION,
+    get_image,
+)
+from custom_components.yandex_weather.updater import WeatherUpdater
 
 none_testdata = [
     ("ThereIsNoSuchKey", "foo"),
@@ -13,3 +18,20 @@ none_testdata = [
 def test_get_none_image(source, condition):
     """Test get_image action."""
     assert get_image(source, condition, "some", False) is None
+
+
+@pytest.mark.asyncio
+async def test_yandex_image(hass):
+    """Test is image correct for Yandex image provider.
+
+    Like in `weather` component.
+    """
+
+    w = WeatherUpdater(0, 0, "", hass, "test_device")
+    await w.async_request_refresh()
+    assert "https://yastatic.net/weather/i/icons/funky/dark/bkn_d.svg" == get_image(
+        image_source="Yandex",
+        condition=w.data.get(ATTR_API_ORIGINAL_CONDITION),
+        is_day=w.data.get("daytime") == "d",
+        image=w.data.get(ATTR_API_IMAGE),
+    )
