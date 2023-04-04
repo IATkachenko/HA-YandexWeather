c6deb4f6b001f935331e46ab1ac03a5d1f3329a7
Igor Tkachenko <IATkachenko@gmail.com>
2023-04-04 15:36:09 +0300

    test: allow load different test data for requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,15 @@
 from unittest.mock import AsyncMock, patch
 
 import pytest
+from _pytest.fixtures import SubRequest
 from pytest_homeassistant_custom_component.common import load_fixture
 
 
-@pytest.fixture(name="_bypass_get_data", autouse=True)
-def bypass_get_data_fixture():
+@pytest.fixture(name="_bypass_get_data")
+def bypass_get_data_fixture(request: SubRequest):
     """Skip calls to get data from API."""
     with patch(
         "custom_components.yandex_weather.updater.WeatherUpdater.request",
-        AsyncMock(return_value=load_fixture("test_data.json")),
+        AsyncMock(return_value=load_fixture(request.param)),
     ):
         yield
--- tests/unit/test_updater.py
+++ tests/unit/test_updater.py
@@ -4,28 +4,37 @@ import pytest
 from custom_components.yandex_weather.const import ATTR_MIN_FORECAST_TEMPERATURE
 from custom_components.yandex_weather.updater import WeatherUpdater
 
-testdata = [
-    ("condition", "cloudy"),
-    ("daytime", "d"),
-    ("feels_like", -5),
-    ("humidity", 89),
-    ("icon", "bkn_d"),
-    ("min_forecast_temperature", -1),
-    ("original_condition", "cloudy"),
-    ("pressure_mm", 723),
-    ("pressure_pa", 963),
-    ("temp", 1),
-    ("wind_dir", 270),
-    ("wind_gust", 13.9),
-    ("wind_speed", 6.2),
-    ("yandex_condition", "cloudy"),
-    (ATTR_MIN_FORECAST_TEMPERATURE, -1)
-]
+scenarios = {
+    "test_data.json": [
+        ("condition", "cloudy"),
+        ("daytime", "d"),
+        ("feels_like", -5),
+        ("humidity", 89),
+        ("icon", "bkn_d"),
+        ("min_forecast_temperature", -1),
+        ("original_condition", "cloudy"),
+        ("pressure_mm", 723),
+        ("pressure_pa", 963),
+        ("temp", 1),
+        ("wind_dir", 270),
+        ("wind_gust", 13.9),
+        ("wind_speed", 6.2),
+        ("yandex_condition", "cloudy"),
+        (ATTR_MIN_FORECAST_TEMPERATURE, -1),
+    ],
+}
 
 
-@pytest.mark.parametrize("key,value", testdata)
+def pytest_generate_tests(metafunc):
+    tests = []
+    for data, _tests in scenarios.items():
+        tests += ((data, *t) for t in _tests)
+    if "_bypass_get_data" in metafunc.fixturenames:
+        metafunc.parametrize("_bypass_get_data, key, value", tests, indirect=["_bypass_get_data"])
+
+
 @pytest.mark.asyncio
-async def test_update(hass, key, value):
+async def test_update(hass, key, value, _bypass_get_data):
     """Test update action."""
     w = WeatherUpdater(0, 0, "", hass, "test_device")
     await w.async_request_refresh()
