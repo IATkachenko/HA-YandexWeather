dfb90171100109e1cb5257d6d631ae9e2efe66d0
IATkachenko <IATkachenko@gmail.com>
2024-04-07 17:31:38 +0300

    remove unused import

--- custom_components/yandex_weather/weather.py
+++ custom_components/yandex_weather/weather.py
@@ -6,7 +6,6 @@ from datetime import datetime, timezone
 import logging
 
 from homeassistant.components.weather import (
-    ATTR_FORECAST_IS_DAYTIME,
     ATTR_WEATHER_PRECIPITATION_UNIT,
     ATTR_WEATHER_PRESSURE_UNIT,
     ATTR_WEATHER_TEMPERATURE_UNIT,
