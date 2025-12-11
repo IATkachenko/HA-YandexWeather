1d27d58ca15391f61e1754f236ec7d2af96885cc
Denis Shulyaka <Shulyaka@gmail.com>
2025-12-11 12:12:22 +0300

    fix(sensors): use correct state_class for wind direction sensor
    many thanks to @Shulyaka

--- custom_components/yandex_weather/sensor.py
+++ custom_components/yandex_weather/sensor.py
@@ -72,7 +72,7 @@ WEATHER_SENSORS: tuple[SensorEntityDescription, ...] = (
         entity_registry_enabled_default=False,
         icon="mdi:compass-rose",
         translation_key=ATTR_API_WIND_BEARING,
-        state_class=SensorStateClass.WIND_DIRECTION,
+        state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.WIND_DIRECTION,
         native_unit_of_measurement=DEGREE,
     ),
